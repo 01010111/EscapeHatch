62de51db588f4f9c6155d72dd758fda742436395
Will Blanton <will@Wills-MacBook-Pro.local>
2020-02-19 16:04:59 -0500

    add vars for getting cell positions for objects

--- source/objects/GameObject.hx
+++ source/objects/GameObject.hx
@@ -6,6 +6,15 @@ class GameObject extends FlxSprite {
 
 	public static var wait:Bool = false;
 
+	public var cx(get, never):Int;
+	function get_cx() {
+		return (getMidpoint().x / 16).floor();
+	}
+	public var cy(get, never):Int;
+	function get_cy() {
+		return (getMidpoint().y / 16).floor();
+	}
+
 	public function new(x:Float, y:Float) {
 		super(x, y);
 	}
